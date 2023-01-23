7f96a1af14d7b7f618093617849ecfabf4480a9c
Tiago Sabadini <sabadini.tiago@gmail.com>
2023-01-23 08:31:27 -0300

    Update variables.tf

--- terraform-exemplo2/variables.tf
+++ terraform-exemplo2/variables.tf
@@ -4,10 +4,10 @@ variable "network_name" {
   default     = "terraform-network"
 }
 
-variable "centro_custo_rh" {
+variable "centro_custo_tech" {
   description = "Nome da Rede"
   type        = string
-  default     = "rh"
+  default     = "tech"
 }
 
 
